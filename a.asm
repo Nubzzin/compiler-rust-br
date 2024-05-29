06938a6db82134f87eb7d375dc0719d790cd8f09
Nubzzin <matheus.thelas@hotmail.com>
2024-05-29 10:24:14 -0300

    Funcoes de sair e imprimir implementadas

--- a.asm
+++ a.asm
@@ -1,6 +1,5 @@
 section .data
     text0 db "Matheus", 10
-    text1 db "Boboca", 10
 
 section .text
     global _start
@@ -9,11 +8,6 @@ _start:
     mov rdi, 1
     mov rsi, text0
     mov rdx, 8
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text1
-    mov rdx, 7
     syscall
     mov rax, 60
     mov rdi, 69
